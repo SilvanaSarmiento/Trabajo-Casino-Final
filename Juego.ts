4b56944214bc484de38cf14bfe2d1e3c0abb9dd3
FlorHeredia <florheredia95@hotmail.es>
2024-12-02 23:16:06 -0300

    agregué atributo saldo

--- Juego.ts
+++ Juego.ts
@@ -3,9 +3,11 @@ import { Apuesta } from "./Apuesta";
 export abstract class Juego implements Apuesta{
     protected nombreDelJuego: string;
     protected apuestaMinima: number;
- constructor(nombreDelJuego:string, apuestaMinima: number) {
+    protected saldo: number;
+ constructor(nombreDelJuego:string, apuestaMinima: number, saldo:number,) {
         this.nombreDelJuego=nombreDelJuego;
         this.apuestaMinima=apuestaMinima;
+        this.saldo=saldo;
     }
 
     abstract realizarApuesta(valor: number): boolean;
